5e69174429ae59c14035e69a1051da91df5435d4
bobur-96 <bovlogs96@gmail.com>
2026-01-11 12:16:58 +0900

    feat telegram shop

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Card from "./components/card/card";
+import { getData } from "./constants/db";
+
+const courses = getData();
+
+const App = () => {
+  return (
+    <>
+      <h1>Sammi kurslar</h1>
+
+      {/* Cart */}
+      <div className="cards_container">
+        {courses.map((course) => (
+          <Card key={course.id} course={course} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/card/card.jsx
@@ -0,0 +1,38 @@
+import "./card.css";
+
+function Card(props) {
+  const { course } = props;
+  return (
+    <div className="cadr">
+      <span className="card_badge">1</span>
+
+      <div className="image_container">
+        <img
+          src={course.Image}
+          alt={course.title}
+          width={"100%"}
+          height={"230px"}
+        />
+      </div>
+
+      <div className="card_body">
+        <h2 className="card_title">{course.title}</h2>
+        <div className="card_price">
+          {course.price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </div>
+      </div>
+
+      <div className="hr"></div>
+
+      <div className="btn_container">
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
