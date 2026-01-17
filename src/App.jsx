f359429eae0721a29b00e0cded7ea91fcca1e306
bobur-96 <bovlogs96@gmail.com>
2026-01-18 01:18:00 +0900

    feat: queryId

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,9 @@ const App = () => {
         body: JSON.stringify(cartItems),
       });
     } else {
-      telegram.sendData(JSON.stringify(cartItems));
+      telegram.sendData(
+        JSON.stringify({ products: cartItems, queryId: queryId })
+      );
     }
   }, [cartItems]);
 
